de9f44a1dcf4c70c6622c284d3826647659238a6
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-18 11:07:35 +0300

    Completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums= [n for n in num_list if n% 2 ==0]
+    return even_nums
 
 def make_exclamation(sentence_list):
-    pass
+    for i in range(len(sentence_list)):
+        sentence_list[i] = sentence_list[i] + "!"
+    return sentence_list
